0b64704f262b0def88cb9feba992c44f1193b6ca
max.bender1 <max.bender1@ibm.com>
2018-11-06 13:34:34 -0500

    update to and

--- cookbooks/travis_ci_sardonyx/recipes/default.rb
+++ cookbooks/travis_ci_sardonyx/recipes/default.rb
@@ -62,14 +62,15 @@ if node['kernel']['machine'] != 's390x'
   include_recipe 'travis_build_environment::xserver'
 end
 
-if node['kernel']['machine'] != 'ppc64le' || node['kernel']['machine'] != 's390x'
+if node['kernel']['machine'] != 'ppc64le' && node['kernel']['machine'] != 's390x'
   include_recipe 'travis_build_environment::google_chrome'
+  include_recipe 'travis_build_environment::firefox'
+  include_recipe 'travis_phantomjs::2'
   # TODO: Uncomment when the Xenial phantomjs archive exists:
   # https://s3.amazonaws.com/travis-phantomjs/binaries/ubuntu/16.04/x86_64/phantomjs-1.9.8.tar.bz2
   # include_recipe 'travis_phantomjs'
 end
-include_recipe 'travis_build_environment::firefox'
-include_recipe 'travis_phantomjs::2'
+
 
 # HACK: sardonyx-specific shims!
 execute 'ln -svf /usr/bin/hashdeep /usr/bin/md5deep'
